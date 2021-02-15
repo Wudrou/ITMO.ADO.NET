98621b06409e84d7e84b20547f59fbb315575036
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-02-15 13:41:55 +0300

    Добавьте файлы проекта.

---
+++ L_1/DBConnection.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace L_1
+{
+    partial class DBConnection
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DBConnection));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Connect = new System.Windows.Forms.ToolStripButton();
+            this.Disconnect = new System.Windows.Forms.ToolStripButton();
+            this.ListConnection = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ListConnection,
+            this.Connect,
+            this.Disconnect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Connect
+            // 
+            this.Connect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Connect.Image = ((System.Drawing.Image)(resources.GetObject("Connect.Image")));
+            this.Connect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(56, 22);
+            this.Connect.Text = "Connect";
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Disconnect.Image = ((System.Drawing.Image)(resources.GetObject("Disconnect.Image")));
+            this.Disconnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(70, 22);
+            this.Disconnect.Text = "Disconnect";
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // ListConnection
+            // 
+            this.ListConnection.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ListConnection.Image = ((System.Drawing.Image)(resources.GetObject("ListConnection.Image")));
+            this.ListConnection.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ListConnection.Name = "ListConnection";
+            this.ListConnection.Size = new System.Drawing.Size(92, 22);
+            this.ListConnection.Text = "List connection";
+            this.ListConnection.Click += new System.EventHandler(this.ListConnection_Click);
+            // 
+            // DBConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "DBConnection";
+            this.Text = "DBConnection";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Connect;
+        private System.Windows.Forms.ToolStripButton Disconnect;
+        private System.Windows.Forms.ToolStripButton ListConnection;
+    }
+}
+
---
+++ L_1/DBConnection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace L_1
+{
+    public partial class DBConnection : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        //string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Northwind;Data Source=DESKTOP-UC53TPR\SQLEXPRESS";
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+        string testConnect = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+        public DBConnection()
+        {
+            InitializeComponent();
+            this.connection.StateChange += new System.Data.StateChangeEventHandler(this.connection_StateChange);
+        }
+        private void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            Connect.Enabled = (e.CurrentState == ConnectionState.Closed);
+            Disconnect.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch (OleDbException XcpSQL)
+            {
+                foreach (OleDbError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message,
+                    "SQL Error code " + se.NativeError,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+        private void ListConnection_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    MessageBox.Show("name = " + cs.Name);
+                    MessageBox.Show("providerName = " + cs.ProviderName);
+                    MessageBox.Show("connectionString = " + cs.ConnectionString);
+                }
+            }
+        }
+    }
+}
