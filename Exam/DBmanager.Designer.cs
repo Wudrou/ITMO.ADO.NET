785f595e0af9949d45614924e9fdd61d3a530f58
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-02-22 21:33:59 +0300

    add

---
+++ Exam/DBmanager.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Exam
+{
+    partial class DBmanager
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.show = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.change = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox_IDorder = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(100, 10);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(167, 20);
+            this.textBox_ID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID клиента";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(100, 40);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(167, 20);
+            this.textBox_FirstName.TabIndex = 3;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(100, 70);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(167, 20);
+            this.textBox_LastName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Фамилия";
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(286, 10);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(100, 45);
+            this.show.TabIndex = 6;
+            this.show.Text = "Показать данные";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(392, 10);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(100, 45);
+            this.add.TabIndex = 7;
+            this.add.Text = "Добавить данные";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(498, 10);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(100, 45);
+            this.change.TabIndex = 8;
+            this.change.Text = "Редактировать";
+            this.change.UseVisualStyleBackColor = true;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(604, 10);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(100, 45);
+            this.delete.TabIndex = 9;
+            this.delete.Text = "Удалить";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(286, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(418, 251);
+            this.listBox1.TabIndex = 10;
+            // 
+            // textBox_IDorder
+            // 
+            this.textBox_IDorder.Location = new System.Drawing.Point(100, 100);
+            this.textBox_IDorder.Name = "textBox_IDorder";
+            this.textBox_IDorder.Size = new System.Drawing.Size(167, 20);
+            this.textBox_IDorder.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Номер заказа";
+            // 
+            // DBmanager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_IDorder);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.textBox_FirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_ID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "DBmanager";
+            this.Text = "DBmanager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox_IDorder;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Exam/DBmanager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam
+{
+    public partial class DBmanager : Form
+    {
+        public DBmanager()
+        {
+            InitializeComponent();
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            using (MyDBEntities ctx = new MyDBEntities())
+            {
+                foreach (Customers cust in ctx.Customers)
+                    listBox1.Items.Add(cust.PersonID + "  |  " + cust.FirstName + "  |  " + cust.LastName);
+            }
+        }
+    }
+}
+
+
